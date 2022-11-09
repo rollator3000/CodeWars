45ce5bb956abf587c79c268bef1fe44a816283c3
= <kuchenmann93@gmail.com>
2022-11-09 14:06:09 +0100

    added a KATA7 Exercise

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Currently I'm focusing on the languages **R**, **Python** & **SQL**.
 ├── SQL			<- SQL Challenges 
 │  ├── Beginners-Series <- Series from CodeWars to start with SQL --- 06 
 │  ├── 7_Kata	  	    <- Exercises in SQL of difficulity '7'    --- 10
-│  └── 6_Kata	  	    <- Exercises in SQL of difficulity '6'    --- 06
+│  └── 6_Kata	  	    <- Exercises in SQL of difficulity '6'    --- 07
 │
 ├── R 			<- R Challenges
 │  ├── R_Kata_8	  	<- Exercises in R of difficulity '8' --- 10 
--- SQL/6_Kata.sql
+++ SQL/6_Kata.sql
@@ -176,3 +176,32 @@ SELECT
   SUBSTRING(name, '^.+\s(\S+)\s\S+$') AS first_lastname,
   SUBSTRING(name, '^.+\s(\S+)$')      AS second_lastname
 FROM people
+
+# 6-7 SQL Basics: Simple table totaling.
+# For this challenge you need to create a simple query to display each unique clan with their total 
+# points and ranked by their total points. 
+
+# people table schema
+#  name
+#  points
+#  clan
+
+# You should then return a table that resembles below select on
+#	rank
+#	clan
+#	total_points
+#	total_people
+
+# The query must rank each clan by their total_points, you must return each unqiue clan and if there is no clan name 
+# (i.e. it's an empty string) you must replace it with [no clan specified], you must sum the total_points for each clan 
+# and the total_people within that clan.
+# Note The data is loaded from the live leaderboard, this means values will change but also could cause the kata to time 
+# out retreiving the information.
+SELECT RANK() OVER (ORDER BY SUM(points) DESC),
+  	   COALESCE(NULLIF(clan,''), '[no clan specified]') AS clan,
+  	   SUM(points) AS total_points,
+  	   COUNT(*) AS total_people
+  	FROM people 
+  	GROUP BY clan
+  	ORDER BY total_points DESC
+
