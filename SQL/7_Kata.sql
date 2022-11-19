1a050b3c1b83a0dccc5481ae2ad2979ed3fb506e
rollator3000 <kuchenmann93@gmail.com>
2022-11-19 12:14:26 +0100

    cleaned up SQL 8 & 7

--- SQL/7_Kata.sql
+++ SQL/7_Kata.sql
@@ -1,99 +1,97 @@
 # (7) SQL Exercises with 7-KYU
-# 7-1 SQL Basics: Repeat and Reverse
-#     Using our monsters table with the following schema:
-#
-# monsters table schema
+# (7-1) SQL Basics: Repeat and Reverse -----------------------------------------
+# Using our monsters table with the following schema:
+# > "monsters" table schema:
 # 	id
 # 	name
 # 	legs
 # 	arms
 # 	characteristics
 
-# return the following table:
-  #	name
-  #	characteristics
+# Return the following table:
+#	  name
+#	  characteristics
 
-# Where the name is the original string repeated three times (do not add any spaces), 
-# and the characteristics are the original strings in reverse (e.g. 'abc, def, ghi' becomes 'ihg ,fed ,cba').
+# Where the name is the original string repeated three times (don't add spaces), 
+# and the characteristics are the original strings in reverse.
+# (e.g. 'abc, def, ghi' becomes 'ihg ,fed ,cba').
 SELECT REPEAT(name, 3) AS name,
-	   REVERSE(characteristics) AS characteristics
-	   FROM monsters
+	     REVERSE(characteristics) AS characteristics
+FROM monsters
 
-# 7-2 SQL Basics: Maths with String Manipulations
+# (7-2) SQL Basics: Maths with String Manipulations ----------------------------
 # Given a demographics table in the following format:
+# > "demographics" table schema
+#   id
+#	  name
+#	  birthday
+#	  race
 
-# demographics table schema
-  #	id
-  #	name
-  #	birthday
-  #	race
+# Return a single column named calculation where the value is the bit length of
+#  name, added to the number of characters in race.
+SELECT BIT_LENGTH(name) + LENGTH(race) AS calculation 
+FROM demographics
 
-# Return a single column named calculation where the value is the bit length of name, added to the number of characters in race.
-SELECT BIT_LENGTH(name) + LENGTH(race) AS calculation FROM demographics
+# (7-3) SQL Basics: Simple JOIN with COUNT -------------------------------------
+# For this challenge you need to create a simple SELECT statement that will 
+# return all columns from the people table, and join to the toys table so that 
+# you can return the COUNT of the toys
+# > "people" table schema
+# 	id
+#	  name
 
-# 7-3 SQL Basics: Simple JOIN with COUNT
-# For this challenge you need to create a simple SELECT statement that will return all columns from the people table, 
-# and join to the toys table so that you can return the COUNT of the toys
-
-# people table schema
-  #	id
-  #	name
-
-# toys table schema
-  #	id
-  #	name
-  #	people_id
+# > "toys" table schema
+#	  id
+#	  name
+#	  people_id
 
 # You should return all people fields as well as the toy count as "toy_count".
 SELECT *,
-  (SELECT COUNT(*) FROM toys WHERE people.id = toys.people_id) AS toy_count
-   FROM people
+       (SELECT COUNT(*) FROM toys WHERE people.id = toys.people_id) AS toy_count
+FROM people
 
-# 7-4 First and last IP in a network
-# Given a table where users' connections are logged, find the first and the last address of the
-# networks they connected from.
+# (7-4) First and last IP in a network -----------------------------------------
+# Given a table where users' connections are logged, find the first and the last
+# address of the networks they connected from.
 
 # Notes
 # - Order the result by the id column
-# - There's no need to validate anything - it's okay if the user connects from a private network
-#   (You don't need the connection_time field for this task but without it the input data looks too dull)
+# - There's no need to validate anything - it's okay if the user connects from a
+#   private network (You don't need the connection_time field for this task but
+#                    without it the input data looks too dull)
 # - You can read more about IPv4 on Wikipedia 
 
 # Input table
-# ---------------------------------------------
 # |    Table    |     Column      |   Type    |
 # |-------------+-----------------+-----------|
 # | connections | id              | int       |
 # |             | connection_time | timestamp |
 # |             | ip_address      | inet      |
-# ---------------------------------------------
 
 # Output table
-# ------------------------
 # |    Column     | Type |
 # |---------------+------|
 # | id            | int  |
 # | first_address | text |
 # | last_address  | text |
-# ------------------------
 SELECT id,
-	   network(ip_address) as first_address,
+	     network(ip_address) AS first_address,
   	   broadcast(ip_address) AS last_address
-	FROM connections
-	ORDER BY id
+FROM connections
+ORDER BY id
 
-# 7-5 Truncating
+# (7-5) Truncating -------------------------------------------------------------
 # Given the following table 'decimals':
 
-# decimals table schema
-  #	id
-  #	number1
-  #	number2
+# > "decimals" table schema
+#	  id
+#	  number1
+#	  number2
 
 # Return a table with a single column towardzero where the values are the 
 # result of number1 + number2 truncated towards zero.
-SELECT TRUNC(number1+number2) as towardzero
-	FROM decimals
+SELECT TRUNC(number1 + number2) AS towardzero
+FROM decimals
 
 # Alternativly (if you don't know TRUNC)
 SELECT CASE 
@@ -101,99 +99,105 @@ SELECT CASE
              THEN FLOOR(number1 + number2)
           ELSE CEILING(number1 + number2)
        END as towardzero
-	FROM decimals
+FROM decimals
 
-# 7-6 Concatenating Columns
-# Given the table 'names' below:
-  #	id
-  #	prefix
-  #	first
-  #	last
-  #	suffix
+# (7-6) Concatenating Columns --------------------------------------------------
+# > "names"-table schema
+#	  id
+#	  prefix
+#	  first
+#	  last
+#	  suffix
 
-# Your task is to use a select statement to return a single column table containing the full 
-# title of the person (concatenate all columns together except id), as follows (don't forget to add spaces):
-
-# 	title
+# Your task is to use a select statement to return a single column table con-
+# taining the full title of the person (all columns together except id)
 SELECT CONCAT(prefix, ' ', first, ' ', last, ' ', suffix) AS title 
-  from names
+FROM names
 
-# 7-7 GROCERY STORE: Logistic Optimisation
-# You are the owner of the Grocery Store. All your products are in the database, that you have created after CodeWars SQL excercises!:)
-# You have reached a conclusion that you waste to much time because you have to many different warehouse to visit each week.
-# You have to find out how many different type of products you buy from each producer - if you take only few items from some of them you 
-# will stop going there to save the gasoline:)
+# (7-7) GROCERY STORE: Logistic Optimisation -----------------------------------
+# You are the owner of the Grocery Store. All your products are in the database, 
+# that you have created after CodeWars SQL excercises!:)
+# You have reached a conclusion that you waste to much time because you have to 
+# many different warehouse to visit each week. You have to find out how many 
+# different type of products you buy from each producer - if you take only 
+# few items from some of them you will stop going there to save the gasoline:)
 
 # In the results show producer and count_products_types which you buy from him.
-# Order the result by count_products_types (DESC) then by producer (ASC) in case there are duplicate amounts
+# Order the result by count_products_types (DESC) then by producer (ASC) in case
+#  there are duplicate amounts
 
-# products table schema
-  # id
-  # name
-  # price
-  # stock
-  # producer
-
-# results table schema
-  # count_products_types
-  # producer
-SELECT COUNT(stock) AS count_products_types, producer FROM products
-  GROUP BY producer
-  ORDER BY count_products_types DESC, producer ASC
-
-# 7-8 GROCERY STORE: Support Local Products
-# You are the owner of the Grocery Store. All your products are in the database, that you have created after CodeWars SQL excercises!:)
-# You care about local market, and want to check how many products come from United States of America or Canada.
-# Please use SELECT statement and IN to filter out other origins.
-# In the results show how many products are from United States of America and Canada respectively.
-# Order by number of products, descending.
-
-# products table schema
-  # id
-  # name
-  # price
-  # stock
-  # producer
-  # country
+# > "products" table schema
+#   id
+#   name
+#   price
+#   stock
+#   producer
 
 # results table schema
-  # products
-  # country
-SELECT country, COUNT(*) AS products FROM products
-  WHERE country IN ('Canada', 'United States of America')
-  GROUP BY country
-  ORDER BY products DESC
+#   count_products_types
+#   producer
+SELECT COUNT(stock) AS count_products_types, producer 
+FROM products
+GROUP BY producer
+ORDER BY count_products_types DESC, 
+         producer ASC
 
-# 7-9 Easy SQL: Counting and Grouping
+# (7-8) GROCERY STORE: Support Local Products ----------------------------------
+# You are the owner of the Grocery Store. All your products are in the database,
+# that you have created after CodeWars SQL excercises!:) You care about local
+# market, and want to check how many products come from  United States of 
+# America or Canada. Please use SELECT statement and IN to filter out other 
+# origins. In the results show how many products are from United States of 
+# America and Canada respectively. Order by number of products, descending.
+
+# > "products" table schema
+#   id
+#   name
+#   price
+#   stock
+#   producer
+#   country
+
+# results table schema
+#   products
+#   country
+SELECT country, 
+       COUNT(*) AS products 
+FROM products
+WHERE country IN ('Canada', 'United States of America')
+GROUP BY country
+ORDER BY products DESC
+
+# (7-9) Easy SQL: Counting and Grouping ----------------------------------------
 # Given a demographics table in the following format:
 
-# demographics table schema
-  # id
-  # name
-  # birthday
-  # race
+# > "demographics" table schema
+#   id
+#   name
+#   birthday
+#   race
 
 # you need to return a table that shows a count of each race represented, 
 # ordered by the count in descending order as:
 
 # output table schema
-  # race
-  # count
+#   race
+#   count
 SELECT race, 
        COUNT(*) AS count 
-  FROM demographics
-  GROUP BY race 
-  ORDER BY count DESC
+FROM demographics
+GROUP BY race 
+ORDER BY count DESC
 
-# 7-10 SQL Basics: Raise to the Power
+# (7-10) SQL Basics: Raise to the Power ----------------------------------------
 # Given the following table 'decimals':
 
-# decimals table schema
-  # id
-  # number1
-  # number2
+# > "decimals" table schema
+#   id
+#   number1
+#   number2
 
 # Return a table with a single column result which is the output of 
 # number1 raised to the power of number2.
 SELECT POWER(number1, number2) AS result
-  FROM decimals
+FROM decimals
--- SQL/8_Kata.sql
+++ SQL/8_Kata.sql
@@ -1,37 +1,37 @@
 # (8) SQL Exercises with 8-KYU
-# 8-1 Third Angle of a Triangle
-# You are given two interior angles (in degrees) of a triangle.
-# Write a function to return the 3rd.
-# Note: only positive integers will be tested.
+# (8-1) Third Angle of a Triangle --------------------------------------------
+# You are given two interior angles (in degrees) of a triangle. Write a 
+# function to return the 3rd. Note: only positive integers will be tested.
 
-# you are given a table 'otherangle' with columns 'a' and 'b'.
+# You are given a table 'otherangle' with columns 'a' and 'b'.
 # return a table with these columns and your result in a column named 'res'.
 SELECT a,
-	   b,
-	   (180 - (a + b)) AS res
+	     b,
+	     (180 - (a + b)) AS res
 FROM otherangle
 
-# 8-2 String repeat
-# Write a function that accepts an integer n and a string s as parameters, 
-# and returns a string of s repeated exactly n times.
+# (8-2) String repeat --------------------------------------------------------
+# Write a function that accepts an integer n and a string s as parameters and
+# returns a string of s repeated exactly n times.
 
-# Write your SQL statement here: you are given a table 'repeatstr' with columns 'n' and 's', 
-# Return a table with all columns and your result in a column named 'res'.
+# Write your SQL statement here: you are given a table 'repeatstr' with 
+# columns 'n' and 's'. Return a table with all columns and your result in a 
+# column named 'res'.
 SELECT s,
        n,
        REPEAT(s, n) AS res
 FROM repeatstr
 
-# 8-3 Opposite number
+# (8-3) Opposite number ------------------------------------------------------
 # Very simple, given an integer or a floating-point number, find its opposite.
 # Examples:
 # 	  1: -1
 # 	 14: -14
 # 	-34:  34
 
-# You will be given a table: opposite, with a column: number. 
-# Return a table with a column: res.
-# LONG
+# You will be given a table: opposite, with a column: number. Return a table 
+# with a column: res.
+# LONG-Version
 SELECT CASE
         WHEN number = 0 THEN 0
         WHEN number > 0 THEN -number
@@ -39,31 +39,35 @@ SELECT CASE
        END AS res
 FROM opposite
 
-# SHORT
+# SHORT-Version
 SELECT -number AS res 
 FROM opposite
 
-# 8-4 Remove First and Last Character
-# It's pretty straightforward. Your goal is to create a function that removes the first and 
-# last characters of a string. You're given one parameter, the original string. 
-# You don't have to worry with strings with less than two characters.
-# -- You are given a table 'removechar' with column 's', 
-#    return a table with column 's' and your result in a column named 'res'
-SELECT s, CASE 
-           WHEN LENGTH(s) < 3 THEN ''
-           ELSE SUBSTRING(s, 2, LENGTH(s) - 2)
-          END AS res
+# (8-4) Remove First and Last Character --------------------------------------
+# It's pretty straightforward. Your goal is to create a function that removes 
+# the first and  last characters of a string. You're given one parameter, the 
+# original string. You don't have to worry with strings with less than two 
+# characters.
+
+# You are given a table 'removechar' with column 's', return a table with the 
+# column 's' and your result in a column named 'res'
+SELECT s, 
+       CASE 
+        WHEN LENGTH(s) < 3 THEN ''
+        ELSE SUBSTRING(s, 2, LENGTH(s) - 2)
+       END AS res
 FROM removechar
 
-# 8-5 Will you make it?
-# You were camping with your friends far away from home, but when it's time to go back, 
-# you realize that your fuel is running out and the nearest pump is 50 miles away! 
-# You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
-# Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+# (8-5) Will you make it? ----------------------------------------------------
+# You were camping with your friends far away from home, but when it's time to 
+# go back, you realize that your fuel is running out and the nearest pump is 
+# 50 miles away! You know that on average, your car runs on about 25 miles per
+# gallon. There are 2 gallons left. Considering these factors write a function
+#  that tells you if it is possible to get to the pump or not.
 
 # Write your SQL statement here: you are given a table 'zerofuel' with columns 
-# 'distance_to_pump', 'mpg' and 'fuel_left', return a table with all the columns 
-# and your result in a column named 'res'.
+# 'distance_to_pump', 'mpg' and 'fuel_left', return a table with all the 
+# columns and your result in a column named 'res'.
 SELECT distance_to_pump,
        mpg,
        fuel_left,
@@ -73,17 +77,21 @@ SELECT distance_to_pump,
        END AS res
 FROM zerofuel
 
-# 8-6 Is it a palindrome?
-# Write a function that checks if a given string (case insensitive) is a palindrome.
-# You are given a table 'ispalindrome' with column 'str', return a table with column 
-# 'str' and your result in a column named 'res'.
+# (8-6) Is it a palindrome? --------------------------------------------------
+# Write a function that checks if a string (case insensitive) is a palindrome.
+
+# You are given a table 'ispalindrome' with column 'str', return a table with
+# the column 'str' and your result in a column named 'res'.
 SELECT str, 
        REVERSE(LOWER(str)) = LOWER(str) AS res
 FROM ispalindrome
 
-# 8-7 Remove exclamation marks
-# Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-# You are given a table 'removeexclamationmarks' with column 's', return a table with column 's' and your result in a column named 'res'.
+# (8-7) Remove exclamation marks ---------------------------------------------
+# Write function RemoveExclamationMarks which removes all exclamation marks 
+# from a given string.
+
+# You are given a table 'removeexclamationmarks' with column 's', return a 
+# table with column 's' and your result in a column named 'res'.
 SELECT s,
        REPLACE(s, '!', '') AS res
 FROM removeexclamationmarks
